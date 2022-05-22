35427b1170bb59d27fff09b1807c434be8993ac1
Reanna Ishmael <rishmael@fordham.edu>
2022-05-21 23:00:27 -0400

    Added secret key

--- coterieapp/__init__.py
+++ coterieapp/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_session import Session
+from os import getenv
 
 app = Flask(__name__)
+secret_key = getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = secret_key
 
-SESSION_TYPE = 'null'
+SESSION_TYPE = "filesystem"
 app.config.from_object(__name__)
 Session(app)
 
