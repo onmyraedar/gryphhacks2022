b4157ff048662530d4314e1a7f7da3d187f152f5
Reanna Ishmael <rishmael@fordham.edu>
2022-05-22 15:49:01 -0400

    Finished profile and comparison functions

--- coterieapp/generate_profile.py
+++ coterieapp/generate_profile.py
@@ -3,6 +3,7 @@ import json
 #Different categories of YT videos
 CategoryDict = {2 : 'Autos & Vehicles', 1:   'Film & Animation', 10 : 'Music', 15 : 'Pets & Animals', 17 : 'Sports', 18 : 'Short Movies', 19 : 'Travel & Events', 20 : 'Gaming', 21 : 'Videoblogging', 22 : 'People & Blogs', 23 : 'Comedy', 24 : 'Entertainment', 25 : 'News & Politics', 26 : 'Howto & Style', 27 : 'Education', 28 : 'Science & Technology', 29 : 'Nonprofits & Activism',30 : 'Movies', 31 : 'Anime/Animation', 32 : 'Action/Adventure', 33 : 'Classics', 34 : 'Comedy', 35 : 'Documentary', 36 : 'Drama', 37 : 'Family', 38 : 'Foreign', 39 : 'Horror', 40 : 'Sci-Fi/Fantasy', 41 : 'Thriller', 42 : 'Shorts', 43 : 'Shows', 44 : 'Trailers'}
 
+#Generates the user's top 5 categories
 def generate_t5_categories(vids_filepath):
     vids_file = open("{}.json".format(vids_filepath))
     data = json.load(vids_file)
@@ -48,6 +49,19 @@ def generate_t5_categories(vids_filepath):
         acc += 1
     return top5Cat1
 
+#Generates a list of the user's liked videos
+def generate_liked_vids(vids_filepath):
+    vids_file = open("{}.json".format(vids_filepath))
+    data = json.load(vids_file)
+    Likedict = {}
+    for x in data:
+        for video in x:
+            snippet_dict = video["snippet"]
+            categoryId = snippet_dict["categoryId"]
+            vidTitle = snippet_dict["title"]
+            Likedict[vidTitle] = [video["id"], categoryId]
+    return Likedict
+
 def RelevantSubs(subs_filepath):
     acc = 0
     topYoutube1 = {}
@@ -56,7 +70,44 @@ def RelevantSubs(subs_filepath):
     for x in subdata:
         for item in x:
             acc+=1
-            topYoutube1[acc] = item['snippet']['title']
+            topYoutube1[item['snippet']['title']] = 'https://www.youtube.com/channel/'+item['snippet']['resourceId']['channelId']
             if acc == 100:
                 break
     return topYoutube1
+
+def compareSubs(user1subs_filepath, user2subs_filepath):
+    topYoutubeCommon = []
+    topYoutube1 = RelevantSubs(user1subs_filepath)
+    topYoutube2 = RelevantSubs(user2subs_filepath)
+    print(topYoutube1)
+    for channel in topYoutube1.keys():
+        for channel2 in topYoutube2.keys():
+            if channel == channel2:
+                topYoutubeCommon.append(channel)
+    return topYoutubeCommon
+
+def compareLikes(user1vids_filepath, user2vids_filepath):
+    topLikeCommon = []
+    Likedlist1 = generate_liked_vids(user1vids_filepath)#('speedo3133755de2664b819a13198325c366edvids.json')
+    Likedlist2 = generate_liked_vids(user2vids_filepath)#('sphenic8c82df195bd04699b5411acb300510aevids.json')
+    for vid in Likedlist1:
+        for vid2 in Likedlist2:
+            if vid == vid2:
+                topLikeCommon.append(vid)
+    return topLikeCommon
+    
+def videorecs(user1vids_filepath, user2vids_filepath):
+    topCat1 = generate_t5_categories(user1vids_filepath)
+    topcat = list(topCat1.keys())[0]
+    topcatnum = 0
+    for key in CategoryDict:
+        if CategoryDict[key] == topcat:
+            topcatnum = key
+    Likedict2 = generate_liked_vids(user2vids_filepath)
+    Videorecs = {}
+    for key in Likedict2.keys():
+        catid = int(Likedict2[key][1])
+        vidid = str(Likedict2[key][0])
+        if catid == topcatnum:
+            Videorecs[key] = 'https://www.youtube.com/watch?v='+vidid
+    return Videorecs
