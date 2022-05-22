2171e1dcd34b826a8697cbf175359f65ad3adc40
Reanna Ishmael <rishmael@fordham.edu>
2022-05-22 02:40:08 -0400

    Added YouTube API test

--- coterieapp/routes.py
+++ coterieapp/routes.py
@@ -1,21 +1,25 @@
 from coterieapp import app
-from flask import redirect, request, session, url_for
+from flask import jsonify, redirect, render_template, request, session, url_for
+from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import Flow
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 
 CLIENT_SECRETS_FILE = "client_secrets.json"
 SCOPES = ["https://www.googleapis.com/auth/youtube",
 "https://www.googleapis.com/auth/youtube.force-ssl",
 "https://www.googleapis.com/auth/youtube.readonly"
 ]
+API_SERVICE_NAME = "youtube"
+API_VERSION = "v3"
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<p>Homepage<p>"
+    return render_template("home.html")
 
 @app.route("/authenticate")
-def authenticate_user():
+def authenticate():
     flow = Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
     flow.redirect_uri = url_for('oauth2callback',
         _external=True
@@ -58,11 +62,34 @@ def oauth2callback():
     print(credentials.client_id)   
     print(credentials.client_secret) 
     print(credentials.scopes) 
+
     # Clear the session once finished
-    if "credentials" in session:
-        del session["credentials"]
-    if "state" in session:
-        del session["state"]
+    #if "credentials" in session:
+    #    del session["credentials"]
+    #if "state" in session:
+    #    del session["state"]
 
     return redirect(url_for("home"))
 
+@app.route("/test")
+def test():
+    if "credentials" not in session:
+        return redirect("authenticate")
+    credentials = Credentials(**session["credentials"])
+    youtube = build(
+        API_SERVICE_NAME, API_VERSION, credentials=credentials
+    )
+    subscriptions = youtube.subscriptions().list(mine=True, part='snippet', maxResults=50, order='relevance').execute()
+    print(subscriptions)
+    print("Moving on to videos...")
+    videos = youtube.videos().list(part='snippet', myRating='like', maxResults=50).execute()
+    print(videos)
+    session["credentials"] = {
+        "token": credentials.token,
+        "refresh_token": credentials.refresh_token,
+        "token_uri": credentials.token_uri,
+        "client_id": credentials.client_id,
+        "client_secret": credentials.client_secret,
+        "scopes": credentials.scopes
+    }
+    return jsonify(**subscriptions)
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import os
 from coterieapp import app
 
 if __name__ == "__main__":
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
     app.run('localhost', 8080, debug=True)
